2105af8e276aa05a0a42815ace59d0d273a850f0
shreelimbkar <limbkarshree@gmail.com>
2018-07-05 19:05:27 +0530

    configure htmlwebpackplugin
    configure htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const path = require('path');
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: './public/build/bundle.js'
-    }
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/build')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.html'
+        })
+    ]
 }
